1a066f070fc5034d2eef24077369eb3eeca4d5a6
BhavyaTeja <BhavyaTeja7@gmail.com>
2017-02-15 18:51:55 -0600

    Source Code Updated

---
+++ Source/app/src/main/java/tutorial/cs5551/com/translateapp/RegisterActivity.java
@@ -0,0 +1,128 @@
+package tutorial.cs5551.com.GoogleMapsApp;
+
+/**
+ * Created by BhavyaTeja on 2/15/2017.
+ */
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void RegisterCredentials(View v) {
+        EditText usernameCtrl = (EditText) findViewById(R.id.txtr_uname);
+        EditText passwordCtrl = (EditText) findViewById(R.id.txtr_Pwd);
+        EditText EmailCtrl = (EditText) findViewById(R.id.txtr_Email);
+        TextView errorText = (TextView) findViewById(R.id.lbl_Error);
+        String userName = usernameCtrl.getText().toString();
+        String password = passwordCtrl.getText().toString();
+        String Email = EmailCtrl.getText().toString();
+
+        boolean validationFlag = false;
+        //Verify if the username and password are not empty.
+        if (!userName.isEmpty() && !password.isEmpty()) {
+            if (userName.equals("Admin") && password.equals("Admin")) {
+                validationFlag = true;
+
+            }
+        }
+        if (!validationFlag) {
+            errorText.setVisibility(View.VISIBLE);
+        } else {
+            //This code redirects the from login page to the home page.
+            Intent redirect = new Intent(RegisterActivity.this, MapsActivity.class);
+            startActivity(redirect);
+        }
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Register Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
